000e9618e3f8959b7e2e7d65d4417e0c471a13b2
Gilson Tavares Queiroz <gilsontq@yahoo.com>
2022-09-25 19:20:26 -0300

    Criar componentes para o Dashboard

---
+++ app/common/components/girlBox.js
@@ -0,0 +1,39 @@
+angular.module('primeiraApp').component('girlBox', {
+    bindings: {
+        grid: '@',
+        colorClass: '@',
+        name: '@',
+        value: '@',
+        text: '@',
+    },
+    controller: [
+        'gridSystem',
+        function(gridSystem) {
+            this.$onInit = function () {
+                this.gridClasses = gridSystem.toCssClasses(this.grid)
+            }    
+        }
+    ],
+    template: `
+    <div class="{{ $ctrl.gridClasses }}">
+        <div class="box box-widget widget-user">
+            <div class="widget-user-header {{ $ctrl.colorClass }}">
+                <h4 class="widget-user-username">{{ $ctrl.name }}</h4>
+            </div>
+            <div class="widget-user-image">
+                <img class="img-circle" src="/assets/{{ $ctrl.name }}.png" alt="User Avatar">
+            </div>
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-sm-12 border-right">
+                        <div class="description-block">
+                            <h3 class="description-header">{{ $ctrl.value }}</h3>
+                            <span class="description-text">{{ $ctrl.text }}</span>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+    `
+})
